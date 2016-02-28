913e20ffc186becde5fb4be871975d1df0cbdf0c
neucrack <czd666666@gmail.com>
2016-02-28 23:18:01 +0800

    基本界面

--- src/com/neucrack/GUI/PandaTVDanmu.java
+++ src/com/neucrack/GUI/PandaTVDanmu.java
@@ -13,11 +13,23 @@ import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
 
 import com.sun.awt.AWTUtilities;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JList;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class PandaTVDanmu extends JFrame {
 
 	private JPanel contentPane;
 	static Point origin = new Point();
+	private JTextField textField;
 	/**
 	 * Launch the application.
 	 */
@@ -41,11 +53,70 @@ public class PandaTVDanmu extends JFrame {
 	 */
 	public PandaTVDanmu() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 272, 323);
+		setBounds(1100, 250, 272, 323);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.NORTH);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{0, 0, 0, 0, 0};
+		gbl_panel.rowHeights = new int[]{0, 0};
+		gbl_panel.columnWeights = new double[]{0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JButton button_1 = new JButton("✘");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		GridBagConstraints gbc_button_1 = new GridBagConstraints();
+		gbc_button_1.anchor = GridBagConstraints.EAST;
+		gbc_button_1.insets = new Insets(0, 0, 5, 0);
+		gbc_button_1.gridx = 3;
+		gbc_button_1.gridy = 0;
+		panel.add(button_1, gbc_button_1);
+		
+		JLabel label = new JLabel("房间");
+		GridBagConstraints gbc_label = new GridBagConstraints();
+		gbc_label.insets = new Insets(0, 0, 0, 5);
+		gbc_label.anchor = GridBagConstraints.EAST;
+		gbc_label.gridx = 0;
+		gbc_label.gridy = 1;
+		panel.add(label, gbc_label);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 0, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 1;
+		panel.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JButton button = new JButton("连接");
+		GridBagConstraints gbc_button = new GridBagConstraints();
+		gbc_button.insets = new Insets(0, 0, 0, 5);
+		gbc_button.gridx = 2;
+		gbc_button.gridy = 1;
+		panel.add(button, gbc_button);
+		
+		JButton button_2 = new JButton("锁定");
+		GridBagConstraints gbc_button_2 = new GridBagConstraints();
+		gbc_button_2.anchor = GridBagConstraints.EAST;
+		gbc_button_2.gridx = 3;
+		gbc_button_2.gridy = 1;
+		panel.add(button_2, gbc_button_2);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		contentPane.add(scrollPane, BorderLayout.CENTER);
+		
+		JList list = new JList();
+		scrollPane.setViewportView(list);
 		
 		
 		this.setAlwaysOnTop(true);//窗口置顶
